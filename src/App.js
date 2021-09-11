5e8a1f452be66937d86ca582734d135d701f1d82
elcinp <elcinpars1@gmail.com>
2021-09-11 23:56:19 +0300

    using showInfo and icons plus minus

--- src/App.js
+++ src/App.js
@@ -2,22 +2,22 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 
-
 function App() {
-  const[questions,setQuestions] = useState(data)
-
+  const [questions, setQuestions] = useState(data);
   return (
     <main>
-      <div className="container">
+      <div className='container'>
         <h3>questions and answers about login</h3>
-        <section className="info">         
-            {questions.map((question)=> {
-              return <SingleQuestion key={questions.id} {...questions}/>
-            })}
+        <section className='info'>
+          {questions.map((question) => {
+            return (
+              <SingleQuestion key={question.id} {...question}></SingleQuestion>
+            );
+          })}
         </section>
       </div>
     </main>
-  )
+  );
 }
 
 export default App;
